fe9738104797c5d2e4b7ddc15cab5a9ca51ed0cb
hiran-filho <hiranmenezesfilho@gmail.com>
2026-05-29 19:48:33 -0300

    feat(app): Implement root endpoint and HTML exercise response
    feat(schemas): Add Message schema for structured responses
    
    test: Add tests for root and HTML exercise endpoints

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,4 +1,9 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+
+from fast_zero.schemas import Message  # type: ignore
 
 app = FastAPI(
     title='Fast do Zero',
@@ -7,6 +12,27 @@ app = FastAPI(
 )
 
 
-@app.get('/')
+@app.get(
+    '/',
+    status_code=HTTPStatus.OK,
+    response_model=Message,
+)
 def read_root():
-    return {'message': 'Olá Mundo!'}
+    return {
+        'message': 'Olá Mundo!',
+    }
+
+
+@app.get(
+    '/exercicio-html', status_code=HTTPStatus.OK, response_class=HTMLResponse
+)
+def exercicio_aula_02():
+    return """
+    <html>
+      <head>
+        <title>Nosso olá mundo!</title>
+      </head>
+      <body>
+        <h1> Olá Mundo </h1>
+      </body>
+    </html>"""
